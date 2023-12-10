e9ea6599b07c9a6a94a10f4102dcc560929c6128
Wesley Ribeiro <wesleysribeiro97@gmail.com>
2023-12-10 16:11:58 -0300

    Implement complete game mechanics

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -17,15 +17,36 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       handleMovement();
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        var otherObjectTag = other.gameObject.tag;
+
+        if(otherObjectTag == "UntouchableWall")
+        {
+            Time.timeScale = 0;
+            Debug.Log("Game Over, hit wall that was not supposed to touch");
+        }
+        else if(otherObjectTag == "EndingArea")
+        {
+            // TODO: Implement a way to win only when the player vehicle is completely inside ending area
+            Time.timeScale = 0;
+            Debug.Log("You won!!! Congratulations you have reached the final of the map");
+        }
+    }
+
+    void handleMovement()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             // TODO: Needs work here
             transform.Translate(speed * Time.deltaTime * transform.up);
         }
         else
-        {   
+        {
             // Simulating gravity
-            transform.Translate(0, - goDownSpeed * Time.deltaTime, 0);
+            transform.Translate(0, -goDownSpeed * Time.deltaTime, 0);
         }
 
         float rotation = Input.GetAxis("Horizontal");
