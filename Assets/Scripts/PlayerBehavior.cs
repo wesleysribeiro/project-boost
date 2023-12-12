b029f5287a405c7e2fdb55a5de13fef8f3808709
Wesley Ribeiro <wesleysribeiro97@gmail.com>
2023-12-12 19:15:42 -0300

    Add sound effects Improve camera effects

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,11 +9,17 @@ public class PlayerBehavior : MonoBehaviour
     public float rotationSpeed = 100f;
 
     Rigidbody rb;
+    AudioSource boostSrc;
+    AudioSource losingSrc;
+    AudioSource winningSrc;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        boostSrc = GetComponentInChildren<AudioSource>();
+        losingSrc = GameObject.FindWithTag("LosingAudio").GetComponent<AudioSource>();
+        winningSrc = GameObject.FindWithTag("WinningAudio").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,11 +33,13 @@ public class PlayerBehavior : MonoBehaviour
 
         if(otherObjectTag == "UntouchableWall")
         {
+            losingSrc.Play();
             Time.timeScale = 0;
             Debug.Log("Game Over, hit wall that was not supposed to touch");
         }
         else if(otherObjectTag == "EndingArea")
         {
+            winningSrc.Play();
             // TODO: Implement a way to win only when the player vehicle is completely inside ending area
             Time.timeScale = 0;
             Debug.Log("You won!!! Congratulations you have reached the final of the map");
@@ -39,7 +47,12 @@ public class PlayerBehavior : MonoBehaviour
     }
 
     void handleMovement()
-    {
+    {   
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            boostSrc.PlayDelayed(0.2f);
+        }
+
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(thrust * Time.deltaTime * Vector3.up);
