19a5fd88f5cad67433b704d61ae1ac8e3f731041
Wesley Ribeiro <wesleysribeiro97@gmail.com>
2024-01-04 10:54:13 -0300

    Improve code

--- Assets/Scripts/QuitApplication.cs
+++ Assets/Scripts/QuitApplication.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class QuitApplication : MonoBehaviour
 {
     [SerializeField] KeyCode quitKey = KeyCode.Escape;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
